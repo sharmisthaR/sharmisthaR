2f269dc96a4a3c4a79a7248c4770148cbb5ec67f
Zeit <54981593+sharmisthaR@users.noreply.github.com>
2021-07-01 00:09:00 +0530

    Update RegistrationForm.java

--- ClassProject/src/pkgclass/project/RegistrationForm.java
+++ ClassProject/src/pkgclass/project/RegistrationForm.java
@@ -275,7 +275,8 @@ public class RegistrationForm extends javax.swing.JFrame {
 
     private void lb_cancelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_lb_cancelActionPerformed
         // TODO add your handling code here:
-        System.exit(0);
+        mainform mf = new mainform();
+        mf.setVisible(true);
     }//GEN-LAST:event_lb_cancelActionPerformed
 
     private void lb_resetActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_lb_resetActionPerformed
